8453cb6099047c74a9e57d280f1fb459b6136800
CRTVoid <37534893+CRTVoid@users.noreply.github.com>
2025-12-30 15:22:28 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'cineroll-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'style.css',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+// Установка
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Активация и очистка старых кэшей
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Перехват запросов
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
