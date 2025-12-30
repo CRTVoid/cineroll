9bb377c89592f1fe8aa939a24fcd3bbea08c469b
CRTVoid <37534893+CRTVoid@users.noreply.github.com>
2025-12-30 15:48:28 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,33 +6,14 @@ const urlsToCache = [
   '/icon-512.png'
 ];
 
-// Установка
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Активация и очистка старых кэшей
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cache => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
-});
-
-// Перехват запросов
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
