d0df2488020eee5c0d514c2f19000cc5cc1f9707
CRTVoid <37534893+CRTVoid@users.noreply.github.com>
2025-12-30 15:25:36 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
 const CACHE_NAME = 'cineroll-cache-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
+  'index.html',
   'style.css',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png'
+  'icon-192.png',
+  'icon-512.png'
 ];
 
 // Установка
